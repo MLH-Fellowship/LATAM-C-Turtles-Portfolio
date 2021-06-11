05d026acc011c8ba0e4ef90dc1a0e6e5304c7602
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-06-11 18:57:00 -0500

    🚀 Profile done

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, json
+import os
+
 
 app = Flask(__name__)
 
@@ -6,4 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# code by kOssi (https://stackoverflow.com/questions/21133976/flask-load-local-json)
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(SITE_ROOT, "static/data", "saul.json")
+data = json.load(open(json_url))
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', data=data)
