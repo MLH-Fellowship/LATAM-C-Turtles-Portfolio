2ca89bd677e3a75198cf72fe46143b7cdeac046a
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-06-11 20:14:26 -0500

    🚀 Button go back and CSS

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, url_for, json
 import os
 
-
 app = Flask(__name__)
+
+# code by kOssi (https://stackoverflow.com/questions/21133976/flask-load-local-json)
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+SITE_FOLDER = "static/data"
 
 @app.route('/')
 def index():
     return render_template('landing_page.html')
 
-# code by kOssi (https://stackoverflow.com/questions/21133976/flask-load-local-json)
-SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-json_url = os.path.join(SITE_ROOT, "static/data", "saul.json")
-data = json.load(open(json_url))
-
-@app.route('/profile')
-def profile():
+@app.route('/profile/<name>')
+def profile(name):
+    json_url = os.path.join(SITE_ROOT, SITE_FOLDER, f"{name}.json")
+    data = json.load(open(json_url))
     return render_template('profile.html', data=data)
